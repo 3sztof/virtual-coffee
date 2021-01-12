30ad8936f422b9c0f09d61666cdecbf541b0d25a
Krzysztof Wilczynski <krzywil@amazon.com>
2021-01-12 14:45:28 +0100

    Add optional organizer preference to the script

--- random-team.py
+++ random-team.py
@@ -10,9 +10,21 @@ users = input("List of usernames separated by whitespaces: ").split(" ")
 
 number_of_teams = len(users) // desired_team_size
 
+print("There will be {0} teams according to the entered data".format(number_of_teams))
+
+organizers = input("List of organizers separated by whitespaces (optional, must be as many as the number of teams): ").split(" ")
+
 teams = []
 for _ in range(number_of_teams):
     teams.append([])
+
+if len(organizers) == number_of_teams:
+    for team in teams:
+        organizer = organizers.pop()
+        team.append(organizer)
+        users.remove(organizer)
+else:
+    raise ValueError("You have supplied a list of organizers that is different in length than the number of teams")
 
 for team in teams:
     while len(team) < desired_team_size:
